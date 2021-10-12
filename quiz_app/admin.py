d22545e3d5b3dfa4caf850539a3381a6aa11ccf7
JKK86 <j.kurowski86@wp.pl>
2021-10-13 01:24:06 +0200

    Register Category, Quiz, Question and Answer Models in admin panel

--- quiz_app/admin.py
+++ quiz_app/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
-# Register your models here.
+from quiz_app.models import Category, Quiz, Question, QuestionOrder, Answer
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', ]
+    prepopulated_fields = {'slug': ('name', )}
+
+
+class QuestionOrderAdminInline(admin.TabularInline):
+    model = QuestionOrder
+    extra = 2
+
+
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'time', 'author', 'created', 'updated']
+    exclude = ['created', 'updated', ]
+    inlines = [QuestionOrderAdminInline, ]
+    list_filter = ['category', 'author']
+    search_fields = ['title', 'description', ]
+    list_editable = ['time', ]
+    ordering = ['-created']
+
+
+class AnswerInline(admin.TabularInline):
+    model = Answer
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['content', ]
+    fields = ['content', 'image', ]
+    inlines = [AnswerInline, ]
+    search_fields = ['content', ]
