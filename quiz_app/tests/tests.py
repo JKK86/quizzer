352133bfd9630ccea89b71301c21fe4cb8fc461e
JKK86 <j.kurowski86@wp.pl>
2021-10-14 14:11:28 +0200

    Create test for quiz list by category view

--- quiz_app/tests/tests.py
+++ quiz_app/tests/tests.py
@@ -7,3 +7,11 @@ def test_list_quizzes(client, set_up):
     assert response.status_code == 200
     assert len(set_up[0]) == len(response.context['categories'])
     assert len(set_up[1]) == len(response.context['quizzes'])
+
+
+@pytest.mark.django_db
+def test_list_quizzes_by_category(client, set_up):
+    category = set_up[0][0]
+    response = client.get(f'/{category.slug}/')
+    assert response.status_code == 200
+    assert category.quizzes.count() == len(response.context['quizzes'])
