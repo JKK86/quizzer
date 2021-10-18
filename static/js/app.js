bc8b2fafbf799820b8cecb6b982592b57ac5ba2a
JKK86 <j.kurowski86@wp.pl>
2021-10-18 15:58:54 +0200

    Set quiz timer and create countdown function

--- static/js/app.js
+++ static/js/app.js
@@ -4,26 +4,21 @@ document.addEventListener("DOMContentLoaded", function () {
         /**
          * AJAX to get the questions and answers for the quiz
          */
-        let startButton = document.querySelector("#start-quiz");
+        let startButton = $("#start-quiz");
         let quizContent = document.querySelector("#quiz-content");
 
-        startButton.addEventListener("click", evt => {
-            const url = window.location.href
-            $.ajax({
-                url: `${url}data`,
-                method: "GET",
-                success: function (data) {
-                    const questions = data.questions
-                    questions.forEach((el, i) => {
-                        for (const [question, answers] of Object.entries(el)) {
-                            quizContent.innerHTML += `
+        function showQuestions(data) {
+            const questions = data.questions;
+            questions.forEach((el, i) => {
+                for (const [question, answers] of Object.entries(el)) {
+                    quizContent.innerHTML += `
                         <hr>
                         <div class="question">
-                        <b>${i+1}. ${question}</b>
+                        <b>${i + 1}. ${question}</b>
                         </div>
-                    `
-                            answers.forEach(answer => {
-                                quizContent.innerHTML += `
+                    `;
+                    answers.forEach(answer => {
+                        quizContent.innerHTML += `
                                 <div>
                                 <input type="radio" 
                                 class="answer" 
@@ -32,16 +27,60 @@ document.addEventListener("DOMContentLoaded", function () {
                                 value="${answer}">
                                 <label for="${question}-${answer}">${answer}</label>
                                 </div>
-                                `
-                            })
-                        }
-                    })
-                },
-                error: function (error) {
-                    console.log(error)
+                                `;
+                    });
                 }
-            })
-        })
-    })()
+            });
+        }
 
+        startButton.on("click", evt => {
+            const url = window.location.href;
+            $.ajax({
+                url: `${url}data`,
+                method: "GET",
+                success: showQuestions,
+                error: function (error) {
+                    console.log(error);
+                }
+            });
+        })
+
+        /**
+         * Set quiz timer and start countdown
+         */
+
+        let timerDisplay = $("#quiz-time-left");
+        let timeLeft = timerDisplay.text();
+
+        function countDown() {
+
+            timerDisplay.parent().show();
+            setInterval(function () {
+                if (timeLeft <= 0) {
+                    clearInterval(timeLeft = 0);
+                }
+                if (timeLeft === 0) {
+                    $(".answer").attr("disabled", true);
+                }
+                timeLeft--;
+                timerDisplay.text(timeLeft);
+            }, 1000);
+        }
+
+        startButton.on("click", evt => {
+            if (timeLeft) {
+                countDown();
+            }
+        });
+
+        /**
+         * Hide start button and show submit button
+         */
+
+        startButton.on("click", evt => {
+            startButton.hide();
+            $('#button-submit').show();
+        })
+
+    })()
 });
